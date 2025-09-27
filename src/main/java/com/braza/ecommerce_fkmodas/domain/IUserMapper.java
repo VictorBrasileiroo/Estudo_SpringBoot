b3525a7be65765fcdf272912ac5d547570126499
Your Name <victor.lopesvfk11@gmail.com>
2025-09-27 13:40:37 -0300

    refactor: remove redundant mapping annotations in IUserMapper

--- src/main/java/com/braza/ecommerce_fkmodas/domain/IUserMapper.java
+++ src/main/java/com/braza/ecommerce_fkmodas/domain/IUserMapper.java
@@ -4,19 +4,10 @@ import org.mapstruct.*;
 @Mapper(componentModel = "spring")
 public interface IUserMapper
 {
-    //Funcao que Converte um Dto em uma entidade
-    @Mapping(source = "name", target = "name")
-    @Mapping(source = "phone", target = "phone")
     UserModel toEntity(UserDto userDto);
-
-    //Funcao que Converte uma entidade em um Dto
-    @Mapping(source = "name", target = "name")
-    @Mapping(source = "phone", target = "phone")
     UserDto toDto(UserModel userModel);
 
     //Aqui so atualiza os campos que não são nulos no Dto
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    @Mapping(source = "name", target = "name")
-    @Mapping(source = "phone", target = "phone")
     void updateUserFromDto(UserDto userDto, @MappingTarget UserModel userModel);
 }
